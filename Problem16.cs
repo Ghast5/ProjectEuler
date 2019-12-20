385751c9f228dce232835804f28e9ac9cfce6c19
Damian <ddawdam@gmail.com>
2019-12-20 12:06:09 +0100

    Power digit sum

---
+++ Problem16.cs
@@ -0,0 +1,29 @@
+﻿
+/// <summary>
+/// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+/// What is the sum of the digits of the number 2^1000?
+/// </summary>
+
+using System;
+using System.Numerics;
+
+public class Problem16
+{
+    public BigInteger RaisingToPower() => (BigInteger) Math.Pow(2, 1000);
+
+    public BigInteger AddinNumbers()
+    {
+        BigInteger number = RaisingToPower();
+        string numberLength = number.ToString();
+        BigInteger sum = 0;
+
+        for(int i = 0; i < numberLength.Length; i++)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        
+
+        return sum;
+    }
+}
