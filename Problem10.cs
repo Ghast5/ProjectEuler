45356bafba4a8019a1058b82c521407d84c01ce0
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-17 22:41:04 +0200

    Summation of primes

---
+++ Problem10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem10
+    {
+        public long SummOfPrimes()
+        {
+            int number = 2_000_000;
+
+            bool[] primes = new bool[number + 1];
+
+            for(int i = 2; i <= number; i++)
+                primes[i] = true;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (primes[i])
+                {
+                    int num = i * i;
+
+                    while(num <= number)
+                    {
+                        primes[num] = false;
+                        num += i;
+                    }
+                }
+            }
+
+            long sum = 0;
+
+            for (int i = 2; i < primes.Length; i++)
+            {
+                if (primes[i] == true)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
