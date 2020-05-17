72873aad2fa62f829f35473110690a40cd332260
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-17 20:11:49 +0200

    Smallest multiple

---
+++ Problem5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem5
+    {
+        public int SmallestMultiple()
+        {
+            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19 };
+            int multi = 1;
+
+            for(int i = 0; i < primes.Length; i++)
+            {
+                int res = primes[i];
+
+                while(res < 20)
+                {
+                    res *= primes[i];
+                }
+
+                multi *= res / primes[i];
+            }
+
+            return multi;
+        }
+    }
+}
