252f16c71dacd6e90cf35337345902a803944cf8
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-17 11:39:21 +0200

    Special Pythagorean triplet

---
+++ Problem9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem9
+    {
+        public int PythagoreanTriplet()
+        {
+            for(int a = 1; a < 1000 / 3; a++)
+            {
+                for(int b = a; b < 1000 / 2; b++)
+                {
+                    int c = 1000 - a - b;
+
+                    if(a * a + b * b == c * c)
+                        return a * b * c;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
