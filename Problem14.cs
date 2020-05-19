ad90410fe9abf278482e9b52f9deabd685cdd47c
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-19 18:26:47 +0200

    Longest Collatz Sequence

---
+++ Problem14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem14
+    {
+        public long LongestCollatz()
+        {
+            long chain = 0;
+            long startingNumber = 0;
+            long n; 
+            long tempChain;
+
+            for (int i = 14; i < 1_000_000; i++)
+            {
+                tempChain = 1;
+                n = i;
+
+                while (n != 1)
+                {
+                    if (n % 2 == 0)
+                        n /= 2;
+                    else
+                        n = 3 * n + 1;
+
+                    tempChain++;
+                }
+
+                if (tempChain > chain)
+                {
+                    chain = tempChain;
+                    startingNumber = i;
+                }
+            }
+
+            return startingNumber;
+        }
+    }
+}
