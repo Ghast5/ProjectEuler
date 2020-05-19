a98b3beb3165d26d49a3a484b861f7e5edb03433
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-19 14:24:54 +0200

    10001st prime

---
+++ Problem7.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem7
+    {
+        public long NthPrime()
+        {
+            int number = 100_020_001;
+
+            bool[] primes = new bool[number + 1];
+
+            for (int i = 2; i <= number; i++)
+                primes[i] = true;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (primes[i])
+                {
+                    int num = i * i;
+
+                    while(num <= number)
+                    {
+                        primes[num] = false;
+                        num += i;
+                    }
+                }
+            }
+
+            long position = 0;
+
+            for (int i = 2; i < primes.Length; i++)
+            {
+                if (primes[i] == true)
+                {
+                    position += 1;
+
+                    if (position == 10_001)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return position;
+        }
+    }
+}
