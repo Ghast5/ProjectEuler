933fac98436cf46617acdbbd992cabc8c7ce6995
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-20 22:24:10 +0200

    Digit Fifth Powers

---
+++ Problem30.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    public class Problem30
+    {
+        public int DigitFifthPower()
+        {
+            int result = 0;
+
+            for(int i = 10; i < 355000; i++)
+            {
+                int sumOfPowers = 0;
+                int number = i;
+
+                while(number > 0)
+                {
+                    sumOfPowers += (int) Math.Pow(number % 10, 5);
+                    number /= 10;
+                }
+
+                if(sumOfPowers == i)
+                    result += sumOfPowers;
+            }
+
+            return result;
+        }
+    }
+}
