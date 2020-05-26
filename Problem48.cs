013b2bc600234b8beb8ae017520172a3dafdbb0a
Damian <48653060+Ghast5@users.noreply.github.com>
2020-05-26 18:25:35 +0200

    Self powers

---
+++ Problem48.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Problem48
+    {
+        public BigInteger SelfPowers()
+        {
+            BigInteger sum = 0;
+
+            for(int i = 1; i < 1001; i++)
+                sum = BigInteger.Add(sum, BigInteger.Pow(i, i));
+
+            return BigInteger.Remainder(sum, 10000000000);
+        }
+    }
+}
